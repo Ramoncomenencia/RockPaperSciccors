8bf3fd75288fe42092f8fc920cb731114696be68
Ramon Comenencia <Ramon_Comenencia@hotmail.com>
2025-02-10 18:23:04 +0100

    Add global variables for human and computer score

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+//Global variables
+let computerScore = 0;
+let humanScore = 0;
+
 //Step 1: Setup project strucure
 
 //Step 2: Computer choice
-
-
 function getComputerChoice(){
     
     let random = Math.floor(Math.random() * 3);
@@ -19,7 +21,6 @@ function getHumanChoice(){
     let choice = prompt("Rock, Paper, Sciccors?").toLowerCase();
         
     if(choice === 'rock' || choice === 'paper' || choice === 'sciccors'){
-        
         return choice;
     } else{
         alert('Choose a valid option');
@@ -27,6 +28,10 @@ function getHumanChoice(){
     }
    
 }
+
+//Step 4: Declare players score
+//Step 5: Write single round logic
+
 
 
 
