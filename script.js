cb4b63fea6df513adb8ed0e1bca82eecd1e5d5aa
Ramon Comenencia <Ramon_Comenencia@hotmail.com>
2025-02-14 20:47:23 +0100

    Add playRound function and variables to store human and computer selection in

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function getComputerChoice(){
     
     let random = Math.floor(Math.random() * 3);
     
-    let choice = (random == 0) ? 'Rock':
-    (random == 1) ? 'Paper' : 'Sciccors';
+    let choice = (random == 0) ? 'rock':
+    (random == 1) ? 'paper' : 'sciccors';
 
-    return choice;
+    return choice.at(0).toUpperCase() + choice.slice(1).toLowerCase();
 }
 
 //Step 3: Human choice
@@ -21,19 +21,62 @@ function getHumanChoice(){
     let choice = prompt("Rock, Paper, Sciccors?").toLowerCase();
         
     if(choice === 'rock' || choice === 'paper' || choice === 'sciccors'){
-        return choice;
+        return choice.at(0).toUpperCase() + choice.slice(1).toLowerCase();
     } else{
         alert('Choose a valid option');
         getHumanChoice();
     }
+
+   
    
 }
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
 //Step 4: Declare players score
 //Step 5: Write single round logic
+//Create function playRound
+function playRound(humanChoice, computerChoice){
+    
+  /*   let result = (humanChoice == 'rock' && computerChoice == 'sciccors') ? `You win! ${humanChoice} beats ${computerChoice}` 
+    :   (humanChoice == 'sciccors' && computerChoice == 'paper')  ? `You win! ${humanChoice} beats ${computerChoice}`
+    :   (humanChoice == 'paper' && computerChoice == 'rock')  ? `You win! ${humanChoice} beats ${computerChoice}` 
+    :   (humanChoice == computerChoice) ? "It\'s a tie!" 
+    :   `You lose! ${computerChoice} beats ${humanChoice}`; */
 
 
+    if(humanChoice === 'rock' && computerChoice === 'sciccors'){
 
+        humanScore++;
+        return  console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        
+    } else if(humanChoice === 'paper' && computerChoice === 'rock'){
 
+        humanScore++;
+        return console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+
+    } else if(humanChoice === 'sciccors' && computerChoice === 'paper'){
+
+        humanScore++;
+        return console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        
+    } else if(humanChoice === computerChoice){
+        
+        return  console.log(`It\'s a draw! ${humanChoice} equals ${computerChoice}`)
+    
+    } else{
+
+        computerScore++;
+        return console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+
+    }
+}
+
+playRound(humanSelection, computerSelection);
+console.log(humanScore);
+console.log(computerScore);
+
+//The function than increments the score from whoever won
 
 
